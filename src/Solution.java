e45fc11786660fa150a7416c9a9459c2ca9fdf64
DingZhenyuan <ding_zy@outlook.com>
2021-12-28 21:19:42 -0800

    reaching point

--- src/Solution.java
+++ src/Solution.java
@@ -152,5 +152,19 @@ public class Solution {
         return str.substring(0, i) + choices[j] + str.substring(i+1, 8);
     }
 
+    // Reaching point
+    public boolean reachingPoints(int sx, int sy, int tx, int ty) {
+        while (tx >= sx && ty >= sy) {
+            if (tx == ty) break;
+            if (tx > ty) {
+                if (ty > sy) tx %= ty;
+                else return (tx - sx) % ty == 0;
+            } else {
+                if (tx > sx) ty %= tx;
+                else return (ty - sy) % tx == 0;
+            }
+        }
+        return (tx == sx && ty == sy);
+    }
 
 }
